1ec1a3d4badf35d3da657f9e27f287e66450cce0
Daniel Schmid <daniel@kodal.uk>
2020-05-22 16:02:39 +0100

    Updates

---
+++ precache-manifest.a33f46aff3fb806c184728fddb250663.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1606e0943c315f157947563aa25873f0",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d64ad987d6d09e8930dd",
+    "url": "/roboFriends/static/css/main.c04aa190.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/roboFriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64ad987d6d09e8930dd",
+    "url": "/roboFriends/static/js/main.88a2999a.chunk.js"
+  },
+  {
+    "revision": "db7f08724344aac129ee",
+    "url": "/roboFriends/static/js/runtime-main.ed696bc5.js"
+  }
+]);
